8fb035bf03af0c1638d797b770d41021e6282d28
Brandon Mellus <mellusbrandon@gmail.com>
2021-08-16 22:56:20 -0400

    Add way to delete employees; also, sql.end can accept errors now

--- utils/format-values.js
+++ utils/format-values.js
@@ -1,3 +1,3 @@
 const stringifyVal = require("./stringify-val");
 
-module.exports = vals => vals.map(stringifyVal).join(', ');
+module.exports = vals => vals.map((val) => val !== null ? stringifyVal(val) : 'null').join(', ');
